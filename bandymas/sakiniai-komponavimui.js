a312a117c044ec272f39fe20d149509e974e131b
tadasandriuske <140756969+mskaiciavimas@users.noreply.github.com>
2024-11-11 23:54:15 +0200

    test for language tests

---
+++ bandymas/sakiniai-komponavimui.js
@@ -0,0 +1,38 @@
+const paragraphLibrary = [{ 
+  title: "Vieversys ir rupūžė", 
+  paragraph: "Dievas sumanė sukurti vieversį. Jis paėmė saują molio ir metė aukštyn. Molio gabalėlis pavirto vieversėliu ir ėmė giedoti. Velnias visą tą atsitikimą matė ir panoro padaryti paukštelį. Jis paėmė dumblo saują ir metė aukštyn. Dumblas nukrito atgal. Jis palindo po akmeniu ir pradėjo kurkti. Pats velnias išsigando. Tai buvo rupūžė.", 
+  author: "Pagal lietuvių sakmę",
+  difficulty: "medium "
+   },
+  { 
+  title: "Stasiukas ir voverėlė", 
+  paragraph: "Stasiukas pamatė riešutyne voverėlę. Ji riešutavo. Stasiukui nepatiko. Jis įsigeidė pagauti ir nubausti voverėlę. Stasiukas pasikvietė savo draugą Kaziuką. Jis liepė Kaziukui įlipti į medį ir papurtyti. O pats atsistojo po medžiu ir laukia voverėlės. Įlipo Kaziukas į medį ir purto. O voverėlė strykt iš medžio.", 
+  author: "Pagal Praną Mašiotą",
+  difficulty: "easy"
+   },
+  { 
+  title: "Juozukas ir bitelė", 
+  paragraph: "Juozukas sėdėjo sode. Jis laukė nukrintant obuolio. Čia pat bitelė nuo dobilėlio ant dobilėlio tupinėjo. Ji rinko medutį. Juozukas nusirovė smilgą ir su ja sumanė paerzinti bitelę. Kai bitelė nutupia ant dobilo, ima ir nubraukia ją. Kelis kartus Juozukas bitelę taip nubraukė. Bet jis vis savo darbą dirba. Tik pagaliau bitelės supyko ir įgėlė vaikui į veidą.", 
+  author: "Pagal Praną Mašiotą",
+  difficulty: "medium"
+   },
+  
+  { 
+  title: "Kregždės drąsa", 
+  paragraph: "Kartą vanagas patykojo vištą. Jis čiupo ją ir pakilo į orą. Gaidys spruko į krūmus. Tik mažoji kregždė puolė vytis vagį. Ji pavijo plėšiką ir sučirškė prie pat jo galvos. Vanagas paleido grobį. Ligi šiol vištos ir gaidys svarsto, kokį burtažodį galėjo kregždė vanagui į ausį sukuždėti. Paukščiai nesupranta, jog tik kregždės drąsa išgelbėjo perekšlę.", 
+  author: "Pagal Leonardą Gutauską",
+  difficulty: "medium "
+   },
+  { 
+  title: "Lizdelis po tiltu", 
+  paragraph: "Susisuko kregždė lizdą po tiltu ir išsiperėjo vaikelius. Vaikai paaugo ir savo lizdu buvo nepatenkinti. Tiltu visa laiką važiuoja, dunda ir trinksi, gali į vandenį nukristi. Jie siūlė skristi i pietus. Motina paklausė ir rudenį išskrido su vaikais į šiltuosius kraštus. Pabuvo jie ten per žiemą. Pavasarį kregždė susiruošė skristi atgal. Nors čia šilta ir gera, bet nieko nėra mielesnio už gimtinį lizdelį. Paskui motiną išskrido ir vaikai. Motina sugrįžo į seną savo lizdelį. Kregždžiukai naujus lizdus po tiltu susisuko.", 
+  author: "Pagal Kazį Borutą",
+  difficulty: "hard"
+   },
+  { 
+  title: "Užgavėnės", 
+  paragraph: "Artėjo Užgavėnės. Vaikai darė kaukes, mokėsi dainų. Laimutė su Rūtele pasiuvo didelę Morę. Užgavėnių dieną visi po pamokų išėjo į mokyklos kiemą. Degė laužas. Aplink jį šoko vaikai. Kovėsi Lašininis su Kanapiniu. Skambėjo dainos. Ant laužo degė Morė. Mergaitės vaišino blynais. Linksma šventė – Užgavėnės.", 
+  author: "",
+  difficulty: "easy"
+   },
+  ]  
