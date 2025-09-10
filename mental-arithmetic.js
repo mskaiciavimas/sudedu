41a8fa8f0b81ed3e6db3bdea44fa2e711ac84767
mskaiciavimas <sudedult@gmail.com>
2025-09-10 03:26:16 +0300

    Update mental-arithmetic.js

--- mental-arithmetic.js
+++ mental-arithmetic.js
@@ -514,7 +514,7 @@ async function sendSetTaskResultsToDatabase() {
 
   function triggerFireworks () {
     fireworksDiv.innerHTML = '<img src="../images/sudedu_logo_animation_transparent.gif" style="width: 500px; height: auto;" id="fireworks">';
-    setTimeout(disableFireworks, 4500);
+    //setTimeout(disableFireworks, 4500);
   }
 
   function disableFireworks () {
